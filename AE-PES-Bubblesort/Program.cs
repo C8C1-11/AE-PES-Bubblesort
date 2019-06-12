636134b0fc097309819a20e084c756e3a3667b48
C8C1-11 <45892573+C8C1-11@users.noreply.github.com>
2019-06-12 11:14:16 +0200

    Bubblesort example implemented

---
+++ AE-PES-Bubblesort/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teronis.Tools.NetStandard;
+
+namespace AE_PES_Bubblesort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 1, 5, 2, 4, 3 };
+            IListTools.Bubblesort(array);
+
+            foreach (var number in array)
+                Console.WriteLine(number);
+
+            Console.ReadKey();
+        }
+    }
+}
